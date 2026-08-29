276396a00ae8dfc3dc973a4010ffedb865af02ee
firewall19 <praisecudjoe17@gmail.com>
2026-08-29 13:18:29 +0000

    add square wave carrier signal

---
+++ carrier.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+t = np.linspace(0, 1, 1000)
+fig, ax = plt.subplots()
+line, = ax.plot([], [], lw=2)
+ax.set_xlim(0, 0.5)
+ax.set_ylim(-1.5, 1.5)
+ax.grid(True)
+ax.set_title("Mystic's Square - 5Hz")
+
+def animate(k):
+    sine = np.sin(2 * np.pi * 5 * t + k/10)
+    square = np.where(sine >= 0, 1, -1)
+    line.set_data(t, square)
+    return line,
+
+ani = FuncAnimation(fig, animate, frames=200, interval=50)
+plt.show()
